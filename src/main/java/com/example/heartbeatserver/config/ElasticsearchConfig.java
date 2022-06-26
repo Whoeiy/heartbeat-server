793d247a581f50df4fdb52a4feda2cba01831835
yy <jenniferyue@outlook.com>
2022-06-26 10:56:26 +0800

    yy:更新项目内容

--- src/main/java/com/example/heartbeatserver/config/ElasticsearchConfig.java
+++ src/main/java/com/example/heartbeatserver/config/ElasticsearchConfig.java
@@ -2,15 +2,17 @@ package com.example.heartbeatserver.config;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
+@Configuration
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
     @Override
     public RestHighLevelClient elasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo("101.133.131.175:9200")
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
---
+++ src/main/java/com/example/heartbeatserver/service/EsGiftService.java
@@ -0,0 +1,25 @@
+package com.example.heartbeatserver.service;
+
+import com.example.heartbeatserver.entity.EsGift;
+import com.example.heartbeatserver.util.PageParam;
+import com.example.heartbeatserver.util.PageResult;
+
+import java.util.List;
+
+public interface EsGiftService {
+
+    int importAll();
+
+    void delete(Integer giftId);
+
+    // 根据id创建商品
+    EsGift create(Integer giftId);
+
+    // 简单搜索
+    List<EsGift> simpleSearch(String keyword, PageParam pageParam);
+
+    // 综合搜索
+    List<EsGift> sortSearch(String keyword, PageParam pageParam);
+
+
+}
